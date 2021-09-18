e87478b67a460efcfdae9f315023dbd1ca83d741
Suttirak <51486537+str-Mattaya@users.noreply.github.com>
2021-09-19 00:50:11 +0700

    Checkpoint #1

--- cp1/musicbox.v
+++ cp1/musicbox.v
@@ -11,16 +11,23 @@ reg [3:0] num = 10;
 
 always @(*) begin
 	
+	// check for pressing switch
+	
 	if (sw1 == 0) begin
+		// Pressing S7 set num to 3 -- highest priority
 		num = hundred;
 	end else if (sw2 == 0) begin
+		// Pressing S6 set num to 8
 		num = ten;
 	end else if (sw3 == 0) begin
+		// Pressing S5 set num to 6
 		num = digit;
 	end else begin
+		// Not pressing anything set num to 10 (which means no sound)
 		num = 10;
 	end
 	
+	// choose frequency via num
 	case(num)
 		1: limiter = 25_000_000 / 262; // C4
 		2: limiter = 25_000_000 / 294; // D4
@@ -32,10 +39,12 @@ always @(*) begin
 		8: limiter = 25_000_000 / 524; // C5
 		9: limiter = 25_000_000 / 587; // D5
 		0: limiter = 25_000_000 / 659; // E5
-		default: limiter = 0;
+		default: limiter = 0;		   // No Sound
 	endcase
 	
 end
+
+// clock divider --> input frequency to buzzer
 
 always @(posedge clk) begin
 
